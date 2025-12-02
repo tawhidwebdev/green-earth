2672ab2ea5e77767afbd3fae9b80367fc453888d
tawhidwebdev <tawhidlimon.dev@gmail.com>
2025-12-02 21:02:10 +0600

    categories data

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
-const categoriesList = document.getElementById('categories-list');
+const categoriesContainer = document.getElementById('categories-list');
 const treesContainer =document.getElementById('trees-container');
 
 const categoriesData = async () => {
   try {
     const response = await fetch('https://openapi.programming-hero.com/api/categories');
     const data = await response.json();
-    displayCategories(data.categories);
+    const categories = data.categories;
+    console.log(categories);
+    
+    showCategories(categories);
   } catch (error) {
     console.log('An error occurred while fetching categories:', error);
   }
 }
-const displayCategories = (categories) => {
+
+const showCategories = (categories) => {
   categories.forEach(category => {
-    const li = document.createElement('li');
-    li.classList.add('text-base', 'font-medium', 'px-2.5', 'py-2', 'capitalize', 'pointer');
-    li.innerHTML = `<a>${category.category_name}</a>`;
-    // console.log(category);
-    
-    categoriesList.appendChild(li);
+    categoriesContainer.innerHTML += `
+      <li class="text-base font-medium px-2.5 pt-2 capitalize cursor-pointer">${category.category_name}</li>
+    `
   });
 };
 
-const treesData = async () => {
-  try {
-    const response = await fetch('https://openapi.programming-hero.com/api/plants');
-    const data = await response.json();
-    console.log(data);
-    
-  } catch (error) {
-    console.log('An error occurred while fetching trees:', error);
-  }
-}
-
 categoriesData()
-treesData()
