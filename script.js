676efbdbe35a58a8bf8c1e15318eb818f7db70c7
tawhidwebdev <tawhidlimon.dev@gmail.com>
2025-12-03 22:27:54 +0600

    categories li click function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const categoriesContainer = document.getElementById('categories-list');
-const treesContainer =document.getElementById('trees-container');
+const treesContainer = document.getElementById('trees-container');
 
 const categoriesData = async () => {
   try {
@@ -7,7 +7,7 @@ const categoriesData = async () => {
     const data = await response.json();
     const categories = data.categories;
     console.log(categories);
-    
+
     showCategories(categories);
   } catch (error) {
     console.log('An error occurred while fetching categories:', error);
@@ -17,9 +17,15 @@ const categoriesData = async () => {
 const showCategories = (categories) => {
   categories.forEach(category => {
     categoriesContainer.innerHTML += `
-      <li class="text-base font-medium px-2.5 pt-2 capitalize cursor-pointer">${category.category_name}</li>
+      <li class="text-base font-medium px-2.5 py-2 capitalize rounded cursor-pointer">${category.category_name}</li>
     `
   });
+  categoriesContainer.addEventListener('click', (e) => {
+    const allCtegories = categoriesContainer.querySelectorAll('li');
+    // console.log(allCtegories);
+    allCtegories.forEach(li => li.classList.remove('bg-primary'))
+    console.log(e.target.localName === 'li' ? e.target.classList.add('bg-primary') : '')
+  })
 };
 
 categoriesData()
